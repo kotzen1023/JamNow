f555804a3014dee6c221c955819535db992f5664
richie <kotzen1023@gmail.com>
2018-04-03 17:13:31 +0800

    modify and add

--- app/src/main/java/com/seventhmoon/jamnow/RemoteActivity.java
+++ app/src/main/java/com/seventhmoon/jamnow/RemoteActivity.java
@@ -13,6 +13,9 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 
+import android.support.v7.media.MediaRouteSelector;
+
+import android.support.v7.media.MediaRouter;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
@@ -25,6 +28,18 @@ import android.widget.GridLayout;
 import android.widget.GridView;
 import android.widget.Toast;
 
+import com.google.android.gms.cast.ApplicationMetadata;
+import com.google.android.gms.cast.Cast;
+import com.google.android.gms.cast.CastDevice;
+import com.google.android.gms.cast.CastMediaControlIntent;
+import com.google.android.gms.cast.MediaStatus;
+import com.google.android.gms.cast.RemoteMediaPlayer;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.seventhmoon.jamnow.Data.Constants;
 import com.seventhmoon.jamnow.Data.RemoteServerItem;
 import com.seventhmoon.jamnow.Data.RemoteServerItemArrayAdapter;
@@ -80,6 +95,17 @@ public class RemoteActivity extends AppCompatActivity {
     ActionBar actionBar;
 
     public static Deque<String> pathstack = new ArrayDeque<>();
+    private MediaRouter mMediaRouter;
+    private MediaRouteSelector mMediaRouteSelector;
+    private MediaRouter.Callback mMediaRouterCallback;
+    private RemoteMediaPlayer mRemoteMediaPlayer;
+    private CastDevice mSelectedDevice;
+    private GoogleApiClient mApiClient;
+    private Cast.Listener mCastClientListener;
+    private boolean mWaitingForReconnect = false;
+    private boolean mApplicationStarted = false;
+    private boolean mVideoIsLoaded;
+    private boolean mIsPlaying;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -584,6 +610,176 @@ public class RemoteActivity extends AppCompatActivity {
         Toast toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
         toast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL, 0, 0);
         toast.show();
+    }
+
+    private void initMediaRouter() {
+        // Configure Cast device discovery
+        mMediaRouter = MediaRouter.getInstance( getApplicationContext() );
+        mMediaRouteSelector = new MediaRouteSelector.Builder()
+                .addControlCategory(
+                        CastMediaControlIntent.categoryForCast( "cast" ) )
+                .build();
+        mMediaRouterCallback = new MediaRouterCallback();
+    }
+
+    private void teardown() {
+        if( mApiClient != null ) {
+            if( mApplicationStarted ) {
+                try {
+                    Cast.CastApi.stopApplication( mApiClient );
+                    if( mRemoteMediaPlayer != null ) {
+                        Cast.CastApi.removeMessageReceivedCallbacks( mApiClient, mRemoteMediaPlayer.getNamespace() );
+                        mRemoteMediaPlayer = null;
+                    }
+                } catch( IOException e ) {
+                    //Log.e( TAG, "Exception while removing application " + e );
+                }
+                mApplicationStarted = false;
+            }
+            if( mApiClient.isConnected() )
+                mApiClient.disconnect();
+            mApiClient = null;
+        }
+        mSelectedDevice = null;
+        mVideoIsLoaded = false;
+    }
+
+    private void initCastClientListener() {
+        mCastClientListener = new Cast.Listener() {
+            @Override
+            public void onApplicationStatusChanged() {
+            }
+
+            @Override
+            public void onVolumeChanged() {
+            }
+
+            @Override
+            public void onApplicationDisconnected( int statusCode ) {
+                teardown();
+            }
+        };
+    }
+
+    private void initRemoteMediaPlayer() {
+        mRemoteMediaPlayer = new RemoteMediaPlayer();
+        mRemoteMediaPlayer.setOnStatusUpdatedListener(new RemoteMediaPlayer.OnStatusUpdatedListener() {
+            @Override
+            public void onStatusUpdated() {
+                MediaStatus mediaStatus = mRemoteMediaPlayer.getMediaStatus();
+                mIsPlaying = mediaStatus.getPlayerState() == MediaStatus.PLAYER_STATE_PLAYING;
+            }
+        });
+
+        mRemoteMediaPlayer.setOnMetadataUpdatedListener(new RemoteMediaPlayer.OnMetadataUpdatedListener() {
+            @Override
+            public void onMetadataUpdated() {
+            }
+        });
+    }
+
+    private void reconnectChannels( Bundle hint ) {
+        if( ( hint != null ) && hint.getBoolean( Cast.EXTRA_APP_NO_LONGER_RUNNING ) ) {
+            //Log.e( TAG, "App is no longer running" );
+            teardown();
+        } else {
+            try {
+                Cast.CastApi.setMessageReceivedCallbacks( mApiClient, mRemoteMediaPlayer.getNamespace(), mRemoteMediaPlayer );
+            } catch( IOException e ) {
+                //Log.e( TAG, "Exception while creating media channel ", e );
+            } catch( NullPointerException e ) {
+                //Log.e( TAG, "Something wasn't reinitialized for reconnectChannels" );
+            }
+        }
+    }
+
+    private class ConnectionFailedListener implements GoogleApiClient.OnConnectionFailedListener {
+        @Override
+        public void onConnectionFailed( ConnectionResult connectionResult ) {
+            //teardown();
+        }
+    }
+
+    private void launchReceiver() {
+        Cast.CastOptions.Builder apiOptionsBuilder = Cast.CastOptions
+                .builder( mSelectedDevice, mCastClientListener );
+
+        //ConnectionCallbacks mConnectionCallbacks = new ConnectionCallbacks();
+        ConnectionCallbacks mConnectionCallbacks = new ConnectionCallbacks();
+        ConnectionFailedListener mConnectionFailedListener = new ConnectionFailedListener();
+        mApiClient = new GoogleApiClient.Builder( this )
+                .addApi( Cast.API, apiOptionsBuilder.build() )
+                .addConnectionCallbacks( mConnectionCallbacks )
+                .addOnConnectionFailedListener( mConnectionFailedListener )
+                .build();
+
+        mApiClient.connect();
+    }
+
+    private class ConnectionCallbacks implements GoogleApiClient.ConnectionCallbacks {
+
+        @Override
+        public void onConnected(Bundle hint) {
+            if (mWaitingForReconnect) {
+                mWaitingForReconnect = false;
+                reconnectChannels(hint);
+            } else {
+                try {
+                    Cast.CastApi.launchApplication(mApiClient, "Cast", false)
+                            .setResultCallback(
+                                    new ResultCallback<Cast.ApplicationConnectionResult>() {
+                                        @Override
+                                        public void onResult(
+                                                Cast.ApplicationConnectionResult applicationConnectionResult) {
+                                            Status status = applicationConnectionResult.getStatus();
+                                            if (status.isSuccess()) {
+                                                //Values that can be useful for storing/logic
+                                                ApplicationMetadata applicationMetadata =
+                                                        applicationConnectionResult.getApplicationMetadata();
+                                                String sessionId =
+                                                        applicationConnectionResult.getSessionId();
+                                                String applicationStatus =
+                                                        applicationConnectionResult.getApplicationStatus();
+                                                boolean wasLaunched =
+                                                        applicationConnectionResult.getWasLaunched();
+
+                                                mApplicationStarted = true;
+                                                reconnectChannels(null);
+                                            }
+                                        }
+                                    }
+                            );
+                } catch (Exception e) {
+
+                }
+            }
+        }
+
+        @Override
+        public void onConnectionSuspended(int i) {
+            mWaitingForReconnect = true;
+        }
+    }
+
+    private class MediaRouterCallback extends MediaRouter.Callback {
+
+        @Override
+        public void onRouteSelected(MediaRouter router, MediaRouter.RouteInfo info) {
+            initCastClientListener();
+            initRemoteMediaPlayer();
+
+            mSelectedDevice = CastDevice.getFromBundle( info.getExtras() );
+
+            launchReceiver();
+        }
+
+        @Override
+        public void onRouteUnselected( MediaRouter router, MediaRouter.RouteInfo info ) {
+            teardown();
+            mSelectedDevice = null;
+            //mButton.setText( getString( R.string.play_video ) );
+            mVideoIsLoaded = false;
+        }
     }
 
     private class connectTask extends AsyncTask<Integer, Integer, String>
